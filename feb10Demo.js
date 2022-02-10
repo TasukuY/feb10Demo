6860ae0203ed88630ca2923c01e0d3f9278f51f9
TYama <alohaty81@gmail.com>
2022-02-10 12:51:02 -0700

    demo done

--- feb10Demo.js
+++ feb10Demo.js
@@ -11,7 +11,27 @@ hamburgerBackpack.push(coat)
 
 hamburgerBackpack.pop()
 
-let itemCount = hamburgerBackpack.length
+hamburgerBackpack.push("Extra shoes", "Juice Boxes", "First Aid Kit","Camera", "Garnet's cat")
 
-console.log(itemCount)
-console.log(hamburgerBackpack)
+let hotdogDufflebag = hamburgerBackpack.splice(2, 3)
+if(hamburgerBackpack.length >= 3){
+    for(let i = 0; i < 3; i++){
+        console.log(hamburgerBackpack[i])
+    }
+}else{
+    for(let i = 0; i < hamburgerBackpack.length; i++){
+        console.log(hamburgerBackpack[i])
+    }
+}
+// for(let i = 0; i < hamburgerBackpack.length; i++){
+//     console.log("ham: ", hamburgerBackpack[i])
+// }
+
+// for(let i = 0; i < hotdogDufflebag.length; i++){
+//     console.log("hot: ", hotdogDufflebag[i])
+// }
+
+// let itemCount = hamburgerBackpack.length
+// console.log(itemCount)
+// console.log(`HamburgerBackpack: `, hamburgerBackpack)
+// console.log(`HotdogDufflebag: `, hotdogDufflebag)
